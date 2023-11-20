d36e5ba2f1554121d6d00b9a38af1d6ae354b93c
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-20 14:17:21 +0000

    Add Search component and a backend server.js file

---
+++ bouncing-countries-backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define API route for country information
+app.get('/api/country/:countryName', async (req, res) => {
+  const { countryName } = req.params;
+
+  try {
+    // Use axios to fetch data from the REST Countries API
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`);
+
+    // Send the fetched data back to the frontend
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- bouncing-countries-frontend/src/App.js
+++ bouncing-countries-frontend/src/App.js
@@ -1,21 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Search from "./Search";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
         </p>
+
+        <div>
+
+          
+        </div>
+
+          <h1>
+            This is Bouncing countries
+          </h1>
+
+          <p>
+            Go ahead, bounce a country! What country are you looking for?
+          </p>
+
+          <Search />
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
