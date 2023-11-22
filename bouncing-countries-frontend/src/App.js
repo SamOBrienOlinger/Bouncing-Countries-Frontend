fb4dcda2ea29725b954b39f7eb2688d18ad90ffa
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-22 00:55:28 +0000

    Beginning to combine UX 7 UI with Bounce branding.

--- bouncing-countries-frontend/src/App.js
+++ bouncing-countries-frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Search from "./Search";
@@ -7,12 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code>
-        </p>
 
-        <div>
+        <div id='globe'>
 
           
         </div>
@@ -20,6 +15,26 @@ function App() {
           <h1>
             This is Bouncing countries
           </h1>
+
+          <div>
+            <p> Bounce offers consultancy that delivers the essesntial answers our clients require immediately </p>
+
+            <p> How? </p>
+
+            <span> 
+              <p> By placing their interests at the core of real-time data </p> 
+              <p> The advantage they need to excel </p>
+            </span>
+
+            <p> Our agile research platform guarantees that brands around the globe find their edge </p>
+
+            <h2> Your knowledge-based decisions begin with Bounce </h2>
+
+            <p> Our knowledge becomes yours </p>
+
+            <p> Together we set your industry's standard can only try to reach </p>
+
+          </div>
 
           <p>
             Go ahead, bounce a country! What country are you looking for?
