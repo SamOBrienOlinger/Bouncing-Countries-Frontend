ca2a879629d2c5127007dd2c6488579131ba7259
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-26 12:28:01 +0000

    Tidy up imports

--- bouncing-countries-frontend/src/App.js
+++ bouncing-countries-frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import SearchCountry from './pages/SearchCountry';
-
 // import Navigation from './components/Navigation';
 
 const App = () => {
