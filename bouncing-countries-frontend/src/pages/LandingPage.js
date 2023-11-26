821a4f2905b891299d81dd6fd92a094fd42c795d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-26 12:19:03 +0000

    Remove commented lines

--- bouncing-countries-frontend/src/pages/LandingPage.js
+++ bouncing-countries-frontend/src/pages/LandingPage.js
@@ -22,31 +22,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import Search from './Search'; 
-
-// const LandingPage = () => {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <div>
-//           <h1>This is Bouncing countries</h1>
-//           <p>Go ahead, bounce a country! What country are you looking for?</p>
-//           <Search />
-//         </div>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         ></a>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default LandingPage;
