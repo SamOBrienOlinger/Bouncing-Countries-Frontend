1ff0cbfde7718d86826f3b3af632d104bd6908ca
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-26 14:18:33 +0000

    LandingPage.js updated with Bounce branding

--- bouncing-countries-frontend/src/pages/LandingPage.js
+++ bouncing-countries-frontend/src/pages/LandingPage.js
@@ -10,12 +10,31 @@ const LandingPage = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <h1>This is Bouncing countries</h1>
+
+          <div>
+            <p>Bounce offers consultancy that delivers the essential answers our clients require immediately</p>
+
+            <p>How?</p>
+
+            <div>
+              <p>By placing their interests at the core of real-time data</p>
+              <p>The advantage they need to excel</p>
+            </div>
+
+            <p>Our agile research platform guarantees that brands around the globe find their edge</p>
+
+            <h2>Your knowledge-based decisions begin with Bounce</h2>
+
+            <p>Our knowledge becomes yours</p>
+
+            <p>Together we set your industry's standard can only try to reach</p>
+          </div>
+
           <p>Go ahead, bounce a country! What country are you looking for?</p>
-          {/* <Search /> */}
         </div>
         <Link to="/search" className="App-link">
-  Go to SearchCountry
-</Link>
+          Go to SearchCountry
+        </Link>
       </header>
     </div>
   );
