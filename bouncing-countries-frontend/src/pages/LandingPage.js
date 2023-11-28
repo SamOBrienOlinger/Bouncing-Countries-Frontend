9730521b9fcd201388abf791d725ab2e2e66c3f3
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-28 16:59:09 +0000

    Update styling and add images to pages

--- bouncing-countries-frontend/src/pages/LandingPage.js
+++ bouncing-countries-frontend/src/pages/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/bouncing-countries-logo.png';
+import logo from '../assets/bounce-planet.jpg';
 // import Search from '../components/Search';
 
 const LandingPage = () => {
--- bouncing-countries-frontend/src/pages/SearchCountry.js
+++ bouncing-countries-frontend/src/pages/SearchCountry.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Search from '../components/Search'
+import FourSpheres from '../assets/four-corners-globe.jpg'
+
 
 const SearchCountry = () => {
   return (
     <div>
+      <div id="four-spheres">
+        <img src={FourSpheres}  alt="four spheres" />
+      </div>
+      
       <h1>Search Country Page</h1>
-      <p>This is the search country page.</p>
+
+      <div id="search-intro">
+        <p> Go ahead, Bounce any country off us for global insights! </p>
+        <p> Search below </p>
+      </div>
+      
 
       <Search />
 
