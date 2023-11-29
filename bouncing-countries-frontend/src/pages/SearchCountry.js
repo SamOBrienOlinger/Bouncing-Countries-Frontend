28975ab60ee18348083ad1a760856ce2d3f115eb
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-11-29 20:07:00 +0000

    Add button to bring User back to landing page.

--- bouncing-countries-frontend/src/pages/SearchCountry.js
+++ bouncing-countries-frontend/src/pages/SearchCountry.js
@@ -1,31 +1,56 @@
 import React from 'react';
-import Search from '../components/Search'
-import FourSpheres from '../assets/four-corners-globe.jpg'
-
+import Search from '../components/Search';
+import FourSpheres from '../assets/four-corners-globe.jpg';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Button.module.css';
 
 const SearchCountry = () => {
   return (
-    <div>
-      
-      <a href="https://bounceinsights.com/">
-      <div id="four-spheres">
-        <img src={FourSpheres}  alt="four spheres" />
+    <>
+      <div>
+        <header>
+          <a href="https://bounceinsights.com/">
+            <div id="four-spheres">
+              <img src={FourSpheres} alt="four spheres" />
+            </div>
+          </a>
+
+          <h1>Search Country Page</h1>
+
+          <div id="search-intro">
+            <p> Go ahead, Bounce any country off us for global insights! </p>
+            <p> Search below </p>
+          </div>
+
+          <div className={styles['elementor-button-wrapper']}>
+            <Link
+              to="/search"
+              className={`${styles['elementor-button']} ${styles['elementor-button-link']}`}
+            >
+              <span className={styles['elementor-button-content-wrapper']}>
+                <span className={styles['elementor-button-text']}>
+                  <Search />
+                </span>
+              </span>
+            </Link>
+          </div>
+
+          <div className={styles['elementor-button-wrapper']}>
+            <Link
+              to="/"
+              className={`${styles['elementor-button']} ${styles['elementor-button-link']}`}
+            >
+              <span className={styles['elementor-button-content-wrapper']}>
+                <span className={styles['elementor-button-text']}>
+                  Click Here to Return to the Homepage
+                </span>
+              </span>
+            </Link>
+          </div>
+        </header>
       </div>
-      </a>
-      
-      <h1>Search Country Page</h1>
-
-      <div id="search-intro">
-        <p> Go ahead, Bounce any country off us for global insights! </p>
-        <p> Search below </p>
-      </div>
-      
-
-      <Search />
-
-    </div>
+    </>
   );
 };
 
 export default SearchCountry;
-
